3e060eaef7ff10f98088da74ea6aa485d6174885
Kasper Meerts <kaspermeerts@gmail.com>
2010-12-09 21:48:57 +0100

    Magnets, some graphs and the relevance of current

---
+++ octave/plasma2.m
@@ -0,0 +1,14 @@
+[t,vp,i] = laad_data(93);
+b = bfield(t,vp*4);
+plot(t*1e6,-b,t*1e6,lowpass(i,0.2)/10);
+axis([0, 125, -0.15, 1]);
+
+name='plasma2';
+destdir = '../latex/images';
+relImgDir = 'images';
+ylabrule='0.9cm';
+xlab='$t$ ($\mu$s)';
+ylab='$B$ (T)';
+width='700';
+height='500';
+makeGraph(name,destdir,relImgDir,xlab,ylab,ylabrule,width,height);
